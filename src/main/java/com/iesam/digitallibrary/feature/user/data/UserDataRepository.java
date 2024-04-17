fa64edc7ebf740d63d90349091b5ff8c87e51bce
Rubén Vallejo <146753439+Rubenvalleho@users.noreply.github.com>
2024-04-17 19:17:47 +0200

    feat: Estructura creada

---
+++ src/main/java/com/iesam/digitallibrary/feature/user/data/UserDataRepository.java
@@ -0,0 +1,4 @@
+package com.iesam.digitallibrary.feature.user.data;
+
+public class UserDataRepository {
+}
---
+++ src/main/java/com/iesam/digitallibrary/feature/user/presentation/UserPresentation.java
@@ -0,0 +1,4 @@
+package com.iesam.digitallibrary.feature.user.presentation;
+
+public class UserPresentation {
+}
