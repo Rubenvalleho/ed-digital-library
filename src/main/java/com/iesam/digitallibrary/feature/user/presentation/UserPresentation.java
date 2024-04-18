baf923c238af0d2cca58e86d4ffda639f7661401
Rubén Vallejo <146753439+Rubenvalleho@users.noreply.github.com>
2024-04-18 16:33:57 +0200

    feature: Crear usuario
    Modificaciones en el menú.

--- src/main/java/com/iesam/digitallibrary/feature/user/presentation/UserPresentation.java
+++ src/main/java/com/iesam/digitallibrary/feature/user/presentation/UserPresentation.java
@@ -5,8 +5,26 @@ import com.iesam.digitallibrary.feature.user.data.local.UserFileLocalDataSource;
 import com.iesam.digitallibrary.feature.user.domain.CreateUserUseCase;
 import com.iesam.digitallibrary.feature.user.domain.User;
 
+import java.util.Scanner;
+
 public class UserPresentation {
-    public static void addUser(String userCode, String dni, String name, String lastName, String phoneNumber, String address) {
+    public static void addUser() {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("\nVamos a crear un usuario nuevo.");
+        System.out.println("Introduce el codigo de usuario: ");
+        String userCode = scanner.next();
+        System.out.println("Introduce el dni: ");
+        String dni = scanner.next();
+        System.out.println("Introduce el nombre: ");
+        String name = scanner.next();
+        System.out.println("Introduce los apellidos: ");
+        String lastName = scanner.next();
+        System.out.println("Introduce el numero de telefono: ");
+        String phoneNumber = scanner.next();
+        System.out.println("Introduce su dirección: ");
+        String address = scanner.next();
+
         UserDataRepository userDataRepository = new UserDataRepository(new UserFileLocalDataSource());
         CreateUserUseCase createUserUseCase = new CreateUserUseCase(userDataRepository);
 
