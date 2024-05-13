ffea8573061d25e17a265c291408df4c7df2986a
Rubén Vallejo <146753439+Rubenvalleho@users.noreply.github.com>
2024-05-13 19:16:37 +0200

    test: add user use case - preparacion de clase para hacer test

--- src/test/java/com/iesam/digitallibrary/feature/user/domain/CreateUserUseCaseTest.java
+++ src/test/java/com/iesam/digitallibrary/feature/user/domain/CreateUserUseCaseTest.java
@@ -1,5 +1,13 @@
 package com.iesam.digitallibrary.feature.user.domain;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoFramework;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class CreateUserUseCaseTest {
+    @Mock
+
 
 }
