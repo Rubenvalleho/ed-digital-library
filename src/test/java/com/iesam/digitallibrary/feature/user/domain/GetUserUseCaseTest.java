160177a6d4a49933ee4afe4112a2498fd693d0b5
Rubén Vallejo <rubenvalleho@gmail.com>
2024-05-14 23:03:07 +0200

    test: test GetUserUseCase Test hechos

--- src/test/java/com/iesam/digitallibrary/feature/user/domain/GetUserUseCaseTest.java
+++ src/test/java/com/iesam/digitallibrary/feature/user/domain/GetUserUseCaseTest.java
@@ -1,9 +1,12 @@
 package com.iesam.digitallibrary.feature.user.domain;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,5 +26,30 @@ public class GetUserUseCaseTest {
         getUserUseCase = null;
     }
 
+    @Test
+    public void cuandoIntroduzoUnCodigoValidoObtengoUnUsuarioExistente(){
+        User userExpected = new User("1", "777",
+                "Ruben", "Vallejo", "666", "Calle");
 
+        Mockito.when(userRepository.getUser("1")).thenReturn(userExpected);
+
+        User userReceived = getUserUseCase.execute("1");
+
+        Assertions.assertEquals(userReceived.userCode,"1");
+        Assertions.assertEquals(userReceived.dni,"777");
+        Assertions.assertEquals(userReceived.name,"Ruben");
+        Assertions.assertEquals(userReceived.lastName,"Vallejo");
+        Assertions.assertEquals(userReceived.phoneNumber,"666");
+        Assertions.assertEquals(userReceived.address,"Calle");
+    }
+
+    @Test
+    public void cuandoIntroduzcoUncodigoNoValidoReciboNull() {
+        String notValidUserDni = "002";
+        Mockito.when(userRepository.getUser("002")).thenReturn(null);
+
+        User userReceived = getUserUseCase.execute(notValidUserDni);
+
+        Assertions.assertNull(userReceived);
+    }
 }
