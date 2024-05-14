a35a1af4cfdbf69975dabe2dd9108b52d60f086e
Rubén Vallejo <rubenvalleho@gmail.com>
2024-05-14 13:34:26 +0200

    test: test CreateUserUseCase Se ha implementado el test del caso de uso CreateUserUseCase

--- src/test/java/com/iesam/digitallibrary/feature/user/domain/CreateUserUseCaseTest.java
+++ src/test/java/com/iesam/digitallibrary/feature/user/domain/CreateUserUseCaseTest.java
@@ -1,13 +1,38 @@
 package com.iesam.digitallibrary.feature.user.domain;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoFramework;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class CreateUserUseCaseTest {
     @Mock
+    UserRepository userRepository;
 
+    CreateUserUseCase createUserUseCase;
+
+    @BeforeEach
+    public void setUp() {
+        createUserUseCase = new CreateUserUseCase(userRepository);
+    }
+
+    @AfterEach
+    public void clear() {
+        createUserUseCase = null;
+    }
+
+    @Test
+    public void reciboUnUsuarioYEjecutoElMetodoAddUserParaGuardarlo() {
+        User user = new User("1", "777", "Ruben", "Vallejo", "666", "Calle");
+
+        createUserUseCase.execute(user);
+
+        Mockito.verify(userRepository, Mockito.times(1)).saveUser(user);
+    }
 
 }
