eebe62e0708d0c40cbebb06a9589b565a43e8191
Rubén Vallejo <146753439+Rubenvalleho@users.noreply.github.com>
2024-05-16 00:44:14 +0200

    Testing de añadir usuario. (#51)
    * test: test CreateUserUseCase
    Se ha implementado el test del caso de uso CreateUserUseCase

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
 }
 
 tasks.test {
