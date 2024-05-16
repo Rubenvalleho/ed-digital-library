f167e6fad0c8df417f86f1300e3d9f8a830ccea6
Rubén Vallejo <146753439+Rubenvalleho@users.noreply.github.com>
2024-05-16 15:49:38 +0200

    test: get digitalBook test Test comprobados.

---
+++ src/test/java/com/iesam/digitallibrary/feature/digitalbook/domain/GetDigitalBookUseCaseTest.java
@@ -0,0 +1,46 @@
+package com.iesam.digitallibrary.feature.digitalbook.domain;
+
+import com.iesam.digitallibrary.feature.user.domain.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class GetDigitalBookUseCaseTest {
+
+
+    @Mock
+    DigitalBookRepository digitalBookRepository;
+
+    GetDigitalBookUseCase getDigitalBookUseCase;
+
+    @BeforeEach
+    public void setUp() {
+        getDigitalBookUseCase = new GetDigitalBookUseCase(digitalBookRepository);
+    }
+
+    @AfterEach
+    public void clear() {
+        getDigitalBookUseCase = null;
+    }
+
+    @Test
+    public void reciboUnCodigoYEntoncesDevuelvoUnLibroDigital() {
+        DigitalBook digitalBookExpected = new DigitalBook("1", "Amanecer rojo", "Pierce Brown",
+                "RNA", "Ayer");
+        Mockito.when(digitalBookRepository.getDigitalBook("1")).thenReturn(digitalBookExpected);
+
+        DigitalBook digitalBookReceived = getDigitalBookUseCase.execute("1");
+
+        Assertions.assertEquals(digitalBookReceived.id, digitalBookExpected.id);
+        Assertions.assertEquals(digitalBookReceived.title, digitalBookExpected.title);
+        Assertions.assertEquals(digitalBookReceived.author, digitalBookExpected.author);
+        Assertions.assertEquals(digitalBookReceived.editorial, digitalBookExpected.editorial);
+        Assertions.assertEquals(digitalBookReceived.releaseDate, digitalBookExpected.releaseDate);
+    }
+}
