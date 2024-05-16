3e33f096312ec3506d705c5327bb54dd9f41eaf8
Rubén Vallejo <146753439+Rubenvalleho@users.noreply.github.com>
2024-05-16 15:53:45 +0200

    test: get digitalBook test Test completos.

--- src/test/java/com/iesam/digitallibrary/feature/digitalbook/domain/GetDigitalBookUseCaseTest.java
+++ src/test/java/com/iesam/digitallibrary/feature/digitalbook/domain/GetDigitalBookUseCaseTest.java
@@ -43,4 +43,15 @@ public class GetDigitalBookUseCaseTest {
         Assertions.assertEquals(digitalBookReceived.editorial, digitalBookExpected.editorial);
         Assertions.assertEquals(digitalBookReceived.releaseDate, digitalBookExpected.releaseDate);
     }
+
+    @Test
+    public void cuandoReciboUnCodigoNoValidoEntoncesDevuelvoUnNull() {
+        String notValidId = "2";
+
+        Mockito.when(digitalBookRepository.getDigitalBook("2")).thenReturn(null);
+
+        DigitalBook digitalBookReceived = getDigitalBookUseCase.execute("2");
+
+        Assertions.assertNull(digitalBookReceived);
+    }
 }
